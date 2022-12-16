40786573a793ff8d3bf0afba391616d5caddf8ea
viragdavidcsanad <viragdavidcsanad@gmail.com>
2022-12-16 18:29:34 +0100

    data-frameworks in html cards

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const $memoCard = document.querySelectorAll(".js_memo_card");
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
   this.classList.toggle("flip");
+  if (!hasFilppedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
+  }
 }
 
 $memoCard.forEach((card) => {
