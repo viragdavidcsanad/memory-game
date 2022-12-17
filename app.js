92e286527c7ea5c8461ee1c38a6e709c3249614b
viragdavidcsanad <viragdavidcsanad@gmail.com>
2022-12-17 04:07:22 +0100

    game is ready

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
 const $memoCard = document.querySelectorAll(".js_memo_card");
-let hasFlippedCard = false;
+let hasFlippedCard, lockBoard;
 let firstCard, secondCard;
 
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    resetBoard();
+  }, 1500);
+}
+
+function checkIsMatch() {
+  let isMatchCards =
+    firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatchCards ? disableCards() : unflipCards();
+}
+
 function flipCard() {
+  if (lockBoard || this === firstCard) return;
+
   this.classList.toggle("flip");
-  if (!hasFilppedCard) {
+
+  if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
+    return;
   } else {
     hasFlippedCard = false;
     secondCard = this;
+    checkIsMatch();
   }
 }
 
 $memoCard.forEach((card) => {
   card.addEventListener("click", flipCard);
 });
+
+(function randomizer() {
+  $memoCard.forEach((card) => {
+  rand = Math.floor(Math.random() * 12);
+  card.style.order = rand});
+})()
